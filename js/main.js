9f382c0c6a7cb9d05b3a84a536aeda6741812bae
lipeprado <fi-lipe2010@hotmail.com>
2016-08-26 16:29:16 -0300

    Sessões criadas

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
-$(document).ready(function(){
-	$(".btn-main").on("click",function(){
-
-		$("#inicio").toggleClass('troca');
-
+$(function() {
+	$('a[href*=#]').on('click', function(e) {
+		e.preventDefault();
+		$('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
 	});
-
 });
